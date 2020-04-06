b0bfbb15e818e82933a81998ac773fac1bcb151f
tiya adugna <tiyakefalee@gmail.com>
2020-04-06 06:53:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,15 @@ new_arr = []
 count = 0 
 while src.length > count do
   num = 0 
-  while src[count].length > num do
+  n = 0 
+  while src[count].length - 1 > num do
     n = src[count][num]
-    if n > src[count][num]
-      
+    if n > src[count][num + 1]
+      n = src[count][num + 1]
+    end
+    num += 1 
+  end
+  new_arr << n 
+  count += 1 
+end 
 end
